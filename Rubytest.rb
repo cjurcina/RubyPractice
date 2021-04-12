961a21f1e27b8b80855e475c53528dcfea4d8849
Chris <75489125+cjurcina@users.noreply.github.com>
2021-04-12 01:23:53 -0300

    Add files via upload

---
+++ Rubytest.rb
@@ -0,0 +1,15 @@
+puts "hello, what is your name?";
+name = gets.chomp
+
+case name
+	when "Darth Vader"
+	puts "Welcome, my Lord."
+	when "Tarkin"
+	puts "Welcome, Moff Tarkin."
+	when "Palpatine"
+	puts "Long live the Emperor!"
+end
+
+	
+
+	
